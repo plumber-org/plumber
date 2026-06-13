b2116ccd1c10f4c3dd27c57e16d235de6a355a15
Rishant-BigOh <rishant.kumar@flydocs.aero>
2026-06-13 21:11:20 +0530

    feature: add pre-push hook and setup github actions ci workflow

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <main>
-      <h1>Plumber</h1>
-    </main>
-  );
+    return (
+        <main>
+            <h1>Plumber</h1>
+        </main>
+    );
 }
